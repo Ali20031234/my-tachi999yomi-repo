30ef531836c586961b72b30808d726e9595803d2
Ali20031234 <107797158+Ali20031234@users.noreply.github.com>
2024-12-17 13:29:13 +0300

    Create Ali.kt

---
+++ Ali.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.extension.ar.hentaislayer
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+
+class HentaiSlayer : Madara(
+    "HentaiSlayer", 
+    "https://hentaislayer.net", 
+    "ar"
+)
